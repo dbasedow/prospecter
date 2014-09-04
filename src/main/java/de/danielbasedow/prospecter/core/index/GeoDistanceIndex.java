46d6e1dbc0b9efff4eccc43d66dbd3794d8071ab
Daniel Basedow <daniel.basedow@gmail.com>
2014-09-04 20:56:42 +0200

    switched to customized JSI implementation with support for Long ids. No more alias map for GeoIndex!

--- src/main/java/de/danielbasedow/prospecter/core/index/GeoDistanceIndex.java
+++ src/main/java/de/danielbasedow/prospecter/core/index/GeoDistanceIndex.java
@@ -7,6 +7,7 @@ import de.danielbasedow.prospecter.core.geo.LatLng;
 import gnu.trove.list.array.TIntArrayList;
 import gnu.trove.list.array.TLongArrayList;
 import gnu.trove.procedure.TIntProcedure;
+import gnu.trove.procedure.TLongProcedure;
 import net.sf.jsi.Rectangle;
 import net.sf.jsi.SpatialIndex;
 import net.sf.jsi.rtree.RTree;
@@ -29,13 +30,11 @@ public class GeoDistanceIndex extends AbstractFieldIndex {
      * Tracks maximum distance seen during indexing. Allows reducing the area searched during matching
      */
     private SpatialIndex index;
-    private PostingAliasMap postings;
 
     public GeoDistanceIndex(String name) {
         super(name);
         index = new RTree();
         index.init(null);
-        postings = new PostingAliasMap();
     }
 
     @Override
@@ -45,27 +44,20 @@ public class GeoDistanceIndex extends AbstractFieldIndex {
         for (Token token : tokens) {
             LatLng latLng = (LatLng) token.getToken();
             GeoPerimeter perimeter = new GeoPerimeter(latLng.getLatitude(), latLng.getLongitude(), 0);
-            MatchCollectionProcedure procedure = new MatchCollectionProcedure();
-            index.intersects(perimeter.getRectangle(), procedure);
-
-            for (Integer aliasId : procedure.getHits().toArray()) {
-                postingsFound.add(postings.get(aliasId));
-            }
+            index.intersects(perimeter.getRectangle(), new MatchCollectionProcedure(postingsFound));
         }
         return postingsFound;
     }
 
     @Override
     public void addPosting(Token token, Long posting) {
-        Integer aliasId = postings.aliasPosting(posting);
         GeoPerimeter perimeter = (GeoPerimeter) token.getToken();
-        index.add(perimeter.getRectangle(), aliasId);
+        index.add(perimeter.getRectangle(), posting);
         if (perimeter.spans180Longitude()) {
             //Move this to the matching phase to save memory and make aliases unnecessary
             //if it spans 180° add fake posting on other side of earth
-            aliasId = postings.aliasPosting(posting);
             GeoPerimeter bizarroPerimeter = perimeter.mirrorInFakeSpace();
-            index.add(bizarroPerimeter.getRectangle(), aliasId);
+            index.add(bizarroPerimeter.getRectangle(), posting);
         }
     }
 
@@ -74,60 +66,17 @@ public class GeoDistanceIndex extends AbstractFieldIndex {
         return FieldType.GEO_DISTANCE;
     }
 
+    private class MatchCollectionProcedure implements TLongProcedure {
+        private TLongArrayList hits;
 
-    /**
-     * A single QueryPosting can result in two index entries if it spans from hemispheres at 180/-180
-     * PostingAliasMap supplies an alias id that allows looking up the QueryPosting
-     */
-    private class PostingAliasMap {
-        private HashMap<Integer, Long> postings;
-        private int nextId;
-
-        public PostingAliasMap() {
-            postings = new HashMap<Integer, Long>();
-            nextId = 0;
-        }
-
-        /**
-         * Gets an alias id and maps this id to the query posting. Calling this with the same posting more than once
-         * results in multiple aliases mapping to the same posting.
-         *
-         * @param posting posting to alias
-         * @return alias id
-         */
-        public Integer aliasPosting(Long posting) {
-            Integer id = nextId;
-            nextId++;
-            postings.put(id, posting);
-            return id;
-        }
-
-        /**
-         * Get posting for alias id
-         *
-         * @param id alias id
-         * @return aliased query posting
-         */
-        public long get(Integer id) {
-            return postings.get(id);
-        }
-    }
-
-    private class MatchCollectionProcedure implements TIntProcedure {
-        private TIntArrayList hits;
-
-        public MatchCollectionProcedure() {
-            hits = new TIntArrayList();
+        public MatchCollectionProcedure(TLongArrayList hitList) {
+            hits = hitList;
         }
 
         @Override
-        public boolean execute(int i) {
+        public boolean execute(long i) {
             hits.add(i);
             return true;
-        }
-
-        public TIntArrayList getHits() {
-            return hits;
         }
     }
 }
