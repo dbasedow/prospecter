bd1a5c9af1f54a38ddb3f4935989cae3a6427e2b
Daniel Basedow <daniel.basedow@gmail.com>
2014-09-16 12:41:20 +0200

    add NOT flag to encoded posting

--- src/main/java/de/danielbasedow/prospecter/core/query/QueryPosting.java
+++ src/main/java/de/danielbasedow/prospecter/core/query/QueryPosting.java
@@ -6,6 +6,7 @@ package de.danielbasedow.prospecter.core.query;
 public class QueryPosting {
     public static final int QUERY_ID_INDEX = 0;
     public static final int QUERY_BIT_INDEX = 1;
+    public static final int QUERY_NOT_INDEX = 2;
 
     private final int queryId;
     private final short queryBit;
@@ -24,17 +25,24 @@ public class QueryPosting {
     }
 
     public long getPackedPosting() {
-        return pack(queryId, queryBit);
+        return pack(queryId, queryBit, false);
     }
 
     public static long pack(int queryId, int queryBit) {
-        return (((long) queryId) << 32) | (queryBit & 0xffffffffL);
+        return pack(queryId, queryBit, false);
+    }
+
+    public static long pack(int queryId, int queryBit, boolean notFlag) {
+        int not = notFlag ? 1 : 0;
+
+        return (((long) queryId) << 32) | ((queryBit << 1 ) & 0xfffffffeL) | ((long) not & 0x00000001L);
     }
 
     public static int[] unpack(long posting) {
         return new int[]{
                 (int) (posting >> 32),
-                (int) ((posting) & 0xffffffffl)
+                (int) ((posting) & 0xfffffffel) >> 1,
+                (int) ((posting) & 0x00000001l)
         };
     }
 }
--- src/test/java/de/danielbasedow/prospecter/core/QueryPostingTest.java
+++ src/test/java/de/danielbasedow/prospecter/core/QueryPostingTest.java
@@ -13,19 +13,41 @@ public class QueryPostingTest extends TestCase {
         assertEquals(0, ab[0]);
         assertEquals(0, ab[1]);
 
-        l = QueryPosting.pack(1000, 2000);
+        l = QueryPosting.pack(1000, 2000, true);
         ab = QueryPosting.unpack(l);
         assertEquals(1000, ab[0]);
         assertEquals(2000, ab[1]);
+        assertEquals(1, ab[2]);
 
-        l = QueryPosting.pack(Integer.MAX_VALUE, Integer.MAX_VALUE);
+        l = QueryPosting.pack(1000, 2000, false);
+        ab = QueryPosting.unpack(l);
+        assertEquals(1000, ab[0]);
+        assertEquals(2000, ab[1]);
+        assertEquals(0, ab[2]);
+
+
+        l = QueryPosting.pack(Integer.MAX_VALUE, Integer.MAX_VALUE / 2, true);
         ab = QueryPosting.unpack(l);
         assertEquals(Integer.MAX_VALUE, ab[0]);
-        assertEquals(Integer.MAX_VALUE, ab[1]);
+        assertEquals(Integer.MAX_VALUE / 2, ab[1]);
+        assertEquals(1, ab[2]);
 
-        l = QueryPosting.pack(Integer.MIN_VALUE, Integer.MIN_VALUE);
+        l = QueryPosting.pack(Integer.MAX_VALUE, Integer.MAX_VALUE / 2, false);
+        ab = QueryPosting.unpack(l);
+        assertEquals(Integer.MAX_VALUE, ab[0]);
+        assertEquals(Integer.MAX_VALUE / 2, ab[1]);
+        assertEquals(0, ab[2]);
+
+        l = QueryPosting.pack(Integer.MIN_VALUE, Integer.MIN_VALUE / 2, true);
         ab = QueryPosting.unpack(l);
         assertEquals(Integer.MIN_VALUE, ab[0]);
-        assertEquals(Integer.MIN_VALUE, ab[1]);
+        assertEquals(Integer.MIN_VALUE / 2, ab[1]);
+        assertEquals(1, ab[2]);
+
+        l = QueryPosting.pack(Integer.MIN_VALUE, Integer.MIN_VALUE / 2, false);
+        ab = QueryPosting.unpack(l);
+        assertEquals(Integer.MIN_VALUE, ab[0]);
+        assertEquals(Integer.MIN_VALUE / 2, ab[1]);
+        assertEquals(0, ab[2]);
     }
 }
